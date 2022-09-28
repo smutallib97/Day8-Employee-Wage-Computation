5aa2fa0b6c5dc7588a7611ae6349d8c378eb207b
smutallib97 <smutallib97@gmail.com>
2022-09-28 23:29:34 +0530

    Added: Welcome Message

---
+++ EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employee_wage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
